78f38aad84810246ee7da4536b75d1bd2a7206d1
Chalani Saumya Rajapaksha <chalasaumya@gmail.com>
2023-01-30 00:08:55 +0530

    MongoDB connected

---
+++ src/utils/database.connection.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import config from "../configs";
+import logger from "./logger";
+
+let database;
+
+const connect = async() =>{
+
+    const MONGODB_URL = config.DB_CONNECTION_STRING;  
+
+    if(database) return;    
+
+    mongoose
+        .connect(MONGODB_URL)
+        .then((connection) => {
+          database = connection;
+          logger.info("Database Synced");
+        })
+        .catch((err) => {
+              logger.error(err.message);
+        });
+};
+
+export {connect};
